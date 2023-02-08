5b175403cf6860e60c41bda65b660b0f42e1da3d
unknown <otaviowillker23@gmail.com>
2023-02-08 08:15:09 -0300

    first version 0.1

---
+++ flask_gpt.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+from dotenv import load_dotenv
+import openai, os
+
+load_dotenv()
+
+key_ = os.getenv('KEY_')
+print('TESTEEEEEEE:', key_)
+app = Flask(__name__)
+openai.api_key = key_
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods=['POST'])
+def result():
+    prompt = request.form['prompt']
+    payload = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
+    return payload['choices'][0]['text']
+
+if __name__ == '__main__':
+    app.run()
